89cb99853879b83b916150f6a6ed8c383cfd5737
R. Krishnakumaran <getkrishna96@gmail.com>
2021-05-28 23:18:43 +0530

    minor cosmetic changes to preprocessing codes

---
+++ prepareData.m
@@ -0,0 +1,5 @@
+%% Compute and store PSD and gamma-harmonic phase differences for each hue
+preProcessData
+
+%% Compute and store Gamma power, peak frequency ratios and phase differences for all hues
+getGHratio_GammPower_PD
